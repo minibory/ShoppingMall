bff53b446b50dc4ae5e6afb9c1bda874440bc7ca
CherryOnTop <kaipi10hou@gmail.com>
2022-06-29 16:37:35 +0900

    mysql->mariadb jdbc연결정보 암호화

--- src/test/java/com/kjk/web/WebApplicationTests.java
+++ src/test/java/com/kjk/web/WebApplicationTests.java
@@ -1,5 +1,6 @@
 package com.kjk.web;
 
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,26 @@ class WebApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void jasypt() {
+        String url = "jdbc:mariadb://localhost:3306/kjk_db?serverTimezone=UTC&characterEncoding=UTF-8";
+        String username = "kjk_db";
+        String password = "blue0423";
+
+        System.out.println(jasyptEncoding(url));
+        System.out.println(jasyptEncoding(username));
+        System.out.println(jasyptEncoding(password));
+    }
+
+    public String jasyptEncoding(String value) {
+
+        String key = "blue0423";
+        StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
+        pbeEnc.setAlgorithm("PBEWITHMD5ANDDES");
+        pbeEnc.setPassword(key);
+        return pbeEnc.encrypt(value);
     }
 
 }
