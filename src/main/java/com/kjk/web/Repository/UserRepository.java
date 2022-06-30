b1fa0d960441c5b72c21da7e4d52e2fe0c794ba8
reimex111 <reimex111@gmail.com>
2022-06-30 23:13:11 +0900

    쿼리 수정

--- src/main/java/com/kjk/web/Repository/UserRepository.java
+++ src/main/java/com/kjk/web/Repository/UserRepository.java
@@ -1,10 +1,13 @@
 package com.kjk.web.Repository;
 
 import static com.kjk.web.model.QUser.user;
+
 import com.kjk.web.model.User;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.thymeleaf.util.StringUtils;
 
 import java.util.List;
 
@@ -14,8 +17,16 @@ public class UserRepository {
     private final JPAQueryFactory queryFactory;
 
     public List<User> findById(String loginId) {
-        return queryFactory.selectFrom(user)
-                .where(user.loginId.eq(loginId))
+        return queryFactory
+                .selectFrom(user)
+                .where(eqLoginId(loginId))
                 .fetch();
+    }
+
+    private BooleanExpression eqLoginId(String loginId) {
+        if(StringUtils.isEmpty(loginId)) {
+            return null;
+        }
+        return user.loginId.eq(loginId);
     }
 }
